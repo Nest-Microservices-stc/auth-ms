0717ba30b1c29ad8f9088c6be646c961fe66b129
AngelSt7 <santacruz232004@gmail.com>
2025-08-01 16:48:57 -0500

    feat: add record to envs

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -11,13 +11,15 @@ interface EnvVars {
     NATS_SERVERS: string;
     JWT_SOURCE: JWT_MODE
     JWT_SECRET: string
+    NODE_ENV: 'DEVELOPMENT' | 'PRODUCTION' | 'TESTING';
 }
 
 const envsSchema = joi.object<EnvVars>({
     PORT: joi.number().required(),
     NATS_SERVERS: joi.array().items(joi.string()).required(),
     JWT_SOURCE: joi.string().valid(...Object.values(JWT_MODE)).required(),
-    JWT_SECRET: joi.string().required()
+    JWT_SECRET: joi.string().required(),
+        NODE_ENV: joi.string().valid('DEVELOPMENT', 'PRODUCTION', 'TESTING').required()
 }).unknown(true);
 
 const { error, value } = envsSchema.validate({
@@ -35,5 +37,6 @@ export const envs = {
     port: envVars.PORT,
     natsServers: envVars.NATS_SERVERS,
     jwtSource: envVars.JWT_SOURCE,
-    jwtSecret: envVars.JWT_SECRET
+    jwtSecret: envVars.JWT_SECRET,
+    nodeEnv: envVars.NODE_ENV
 }
